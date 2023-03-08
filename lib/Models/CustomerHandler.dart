8f0f3eabc54481bf3fff9246325c255e974bf982
Arish Khan <arishkhan28.08@gmail.com>
2023-03-08 14:37:15 +0500

    removed unused code.

--- lib/Models/CustomerHandler.dart
+++
@@ -1,9 +0,0 @@
-// import 'customer.dart';
-//
-// class CustomerHandler {
-//   static List<Customer> customers = [
-//     Customer('Abdul Mannan', '56465465132', 0),
-//     Customer('Abdul Hannan', '5665465132', 1),
-//     Customer('Abdul', '5665465132', 2),
-//   ];
-// }
